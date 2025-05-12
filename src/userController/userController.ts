b37164a92d77cc5a5532b2138d977f26b10f488a
Dmytro Zozulia <z1dima1z@gmail.com>
2025-05-12 09:39:29 +0200

    feat: add cluster

--- src/userController/userController.ts
+++ src/userController/userController.ts
@@ -23,37 +23,34 @@ export const createUser = async (req: IncomingMessage, res: ServerResponse) => {
 
       if (!newUser.username || !newUser.age || !newUser.hobbies) {
         sendJson(res, 400, 'Invalid user data');
-        return;
       }
       users.push(newUser);
+      process.send?.({ type: 'createUser', data: newUser });
       sendJson(res, 201, newUser);
     } catch (error) {
       sendJson(res, 400, 'Invalid JSON format');
     }
   });
-  return;
 };
 
 export const getUserById = async (res: ServerResponse, userId: string) => {
   const user = users.find((user) => user.id === userId);
   if (user) {
     sendJson(res, 200, user);
-    return;
   } else {
     sendJson(res, 404, 'User not found');
   }
-  return;
 };
 
 export const deleteUserById = async (res: ServerResponse, userId: string) => {
   const userIndex = users.findIndex((user) => user.id === userId);
   if (userIndex !== -1) {
     users.splice(userIndex, 1);
+    process.send?.({ type: 'deleteUser', data: userId });
     sendJson(res, 200, 'User deleted successfully');
   } else {
     sendJson(res, 404, 'User not found');
   }
-  return;
 };
 
 export const updateUserById = async (
@@ -69,21 +66,17 @@ export const updateUserById = async (
     try {
       const updatedUser: User = JSON.parse(body);
       const user = users.find((user) => user.id === userId);
-
       if (user) {
         user.username = updatedUser.username;
         user.age = updatedUser.age;
         user.hobbies = updatedUser.hobbies;
+        process.send?.({ type: 'updateUser', data: user });
         sendJson(res, 200, user);
-        return;
       } else {
         sendJson(res, 404, 'User not found');
-        return;
       }
     } catch (error) {
       sendJson(res, 400, 'Invalid JSON format');
-      return;
     }
   });
-  return;
 };
